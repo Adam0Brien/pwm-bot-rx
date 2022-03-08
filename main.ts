90b8a0c14a2bc58f8722253d15b3a8c40c6b694e
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-03-08 21:23:02 +0000

    Turning working fine

--- main.ts
+++ main.ts
@@ -1,38 +1,25 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
-        serial.writeLine("Connected")
-    } else {
-        servos.P1.stop()
-    }
-})
 radio.onReceivedValueDeprecated(function (name, value) {
+    led.toggle(2, 4)
+    basic.pause(100)
+    led.toggle(2, 4)
+    basic.pause(100)
     if (name == "left") {
         led.toggle(0, 0)
         led.toggle(0, 0)
         if (value > 20 || value < -20) {
-            servos.P1.setAngle(value)
+            DFRobotMaqueenPlus.servoRun(Servos.S1, value)
             led.plot(0, 0)
             led.unplot(4, 0)
-        } else {
-            servos.P1.stop()
         }
-    } else if (name == "rightf") {
-        led.toggle(1, 0)
-        if (value < 20) {
-            servos.P0.stop()
-        } else {
-            servos.P0.setAngle(value)
-            led.plot(4, 0)
-            led.unplot(0, 0)
+    }
+    if (name == "right") {
+        led.toggle(0, 0)
+        led.toggle(0, 0)
+        if (value > 20 || value < -20) {
+            DFRobotMaqueenPlus.servoRun(Servos.S1, value)
+            led.plot(0, 0)
+            led.unplot(4, 0)
         }
-    } else if (name == "rightr") {
-        servos.P0.setAngle(0 - value)
-        led.plot(4, 0)
-        led.unplot(0, 0)
-    } else if (name == "move") {
-    	
-    } else {
-    	
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -54,7 +41,7 @@ DFRobotMaqueenPlus.mototRun(Motors.M2, Dir.CW, 0)
 DFRobotMaqueenPlus.mototRun(Motors.M1, Dir.CW, 0)
 servos.P0.setAngle(90)
 servos.P1.setAngle(90)
-radio.setGroup(2)
+radio.setGroup(1)
 basic.showString("1 j ")
 /**
  * heartbeat
@@ -64,4 +51,7 @@ basic.forever(function () {
     basic.pause(100)
     led.unplot(2, 2)
     basic.pause(100)
+})
+control.inBackground(function () {
+	
 })
