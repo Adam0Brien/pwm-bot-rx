aa34dc4ddf1b22b9413b03d8d87d77056fd711b9
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-03-16 16:00:29 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,20 @@
 radio.onReceivedValueDeprecated(function (name, value) {
-    timerCounter = 0
     led.toggle(2, 4)
     led.toggle(2, 4)
     if (name == "right") {
+        timerCounter = 0
         led.toggle(0, 0)
         led.toggle(0, 0)
         if (value > 20 || value < -20) {
-            DFRobotMaqueenPlus.servoRun(Servos.S2, value)
             Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo2, value)
             led.plot(0, 0)
             led.unplot(4, 0)
         }
     } else if (name == "left") {
+        timerCounter = 0
         led.toggle(0, 0)
         led.toggle(0, 0)
         if (value > 20 || value < -20) {
-            DFRobotMaqueenPlus.servoRun(Servos.S1, value)
             Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, value)
             led.plot(0, 0)
             led.unplot(4, 0)
@@ -23,9 +22,11 @@ radio.onReceivedValueDeprecated(function (name, value) {
     }
 })
 input.onButtonPressed(Button.A, function () {
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 90)
     DFRobotMaqueenPlus.servoRun(Servos.S1, 0)
 })
 input.onButtonPressed(Button.B, function () {
+    Kitronik_Robotics_Board.servoWrite(Kitronik_Robotics_Board.Servos.Servo1, 0)
     DFRobotMaqueenPlus.servoRun(Servos.S1, 89)
 })
 let timerCounter = 0
